c807c38958b41540495fe164480686991cb5c2ca
Dave Boster <david.boster@amnhealthcare.com>
2022-09-09 15:40:45 -0500

    refactor ＼(๏◡๏)／

--- bowling/Get-Bowling.ps1
+++ bowling/Get-Bowling.ps1
@@ -4,27 +4,23 @@ function Get-Bowling () {
 
 function Bowling-Roll ($scoreCard, $pins) {
     if($pins -gt 10) {
-        throw "Cannot roll more than ten pins"
+        throw "Cannot roll more than ten pins";
     }
     if($pins -lt 0) {
-        throw "Cannot roll less than zero pins"
+        throw "Cannot roll less than zero pins";
     }
     switch ($scoreCard.ball) {
         1 {
             $scoreCard.ball += 1;
-            $scoreCard.frameScore = $pins
-            $scoreCard.score += $scoreCard.frameScore;
+            $scoreCard.frameScore = $pins;
          }
         2 {
             if ($scoreCard.frameScore + $pins -gt 10) {
-                throw "Total pins in a frame can not exceed 10"
+                throw "Total pins in a frame can not exceed 10";
             }
-            $scoreCard.frameScore = $pins;
-            $scoreCard.score += $scoreCard.frameScore;
-            ## Reset Frame
-            $scoreCard.frameScore = 0;
             $scorecard.ball = 1;
         }
     }
-    return $scoreCard
+    $scoreCard.score += $pins;
+    return $scoreCard;
 }
